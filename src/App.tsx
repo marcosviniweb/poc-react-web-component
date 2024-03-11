e8b09ba933572e5713e752668f30e9314a6c3045
MARCOS RIBEIRO <mv.r.marcosvini@gmail.com>
2024-03-10 22:32:05 -0300

    feat: cheboxk

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState, Component } from 'react';
 import './App.css';
-import './assets/seds-checkboxs.js';
+import '@experian-design-system/seds-checkbox';
+import { Item } from '@experian-design-system/seds-checkbox';
 
-
-interface Item {
-  name: string;
-  checked: boolean;
-  position: SedsCheckboxOptions;
-}
-
-
-enum SedsCheckboxOptions {
-  BEFORE = 'before',
-  AFTER = 'after',
-}
-
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'seds-checkbox': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & SEDSCheckboxProps, HTMLElement>;
-    }
-  }
-}
-
-
-interface SEDSCheckboxProps {
-  color?: string;
-  defaultColor?: string;
-  disabled?: boolean;
-  items?: string;
-  label?: string;
-  labelPosition?: string;
-  required?: boolean;
-  tabIndex?: number;
-  onIndeterminateChange?: (updatedItems: Item[]) => void;
-  onModelChange?: (newModel: boolean) => void;
-}
 
 const initialItems: Item[] = [
-  { name: 'Opção 1', checked: false, position: SedsCheckboxOptions.BEFORE },
-  { name: 'Opção 2', checked: false, position: SedsCheckboxOptions.AFTER },
+  { name: 'Opção 1', checked: false, position: 'before' },
+  { name: 'Opção 2', checked: false, position: 'after' },
 ];
 
 const App = () => {
   const [items, setItems] = useState<Item[]>(initialItems);
   const [model, setModel] = useState<boolean>(false);
-  
+
   const itemCheckbox = JSON.stringify(items);
+  const checkboxRef = useRef<HTMLElement>(null); 
 
   useEffect(() => {
+    const checkboxElement = checkboxRef.current;
 
-    const handleIndeterminateChange = (event: CustomEvent) => {
-      console.log('Indeterminate state changed for items:', event.detail);
-      setItems(event.detail);
-    };
+    if (checkboxElement) {
+      const handleIndeterminateChange = (event: CustomEvent) => {
+        console.log('Indeterminate state changed for items:', event.detail);
+        setItems(event.detail);
+      };
 
-    const handleModelChange = (event: CustomEvent) => {
-      console.log('Model changed:', event.detail);
-      setModel(event.detail);
-    };
+      const handleModelChange = (event: CustomEvent) => {
+        console.log('Model changed:', event.detail);
+        setModel(event.detail);
+      };
 
-    const checkboxElement = document.querySelector('seds-checkbox')!;
+      checkboxElement.addEventListener('indeterminateChange', handleIndeterminateChange as EventListener);
+      checkboxElement.addEventListener('modelChange', handleModelChange as EventListener);
 
-    checkboxElement.addEventListener('indeterminateChange', handleIndeterminateChange as EventListener);
-    checkboxElement.addEventListener('modelChange', handleModelChange as EventListener);
+      return () => {
+        checkboxElement.removeEventListener('indeterminateChange', handleIndeterminateChange as EventListener);
+        checkboxElement.removeEventListener('modelChange', handleModelChange as EventListener);
+      };
+    }
+  }, [checkboxRef]);
 
-    return () => {
-      checkboxElement.removeEventListener('indeterminateChange', handleIndeterminateChange as EventListener);
-      checkboxElement.removeEventListener('modelChange', handleModelChange as EventListener);
-    };
-  }, []);
 
   return (
     <header className="App-header">
       <seds-checkbox
+        ref={checkboxRef}
         label='Checkbox do Seds'
         items={itemCheckbox}
         disabled={false}
